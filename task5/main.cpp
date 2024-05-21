07e25ff14dc270603aec92d1b2be89df2309ad1b
Ariuna Bairova <bairova.at@phystech.edu>
2024-05-21 11:12:54 +0300

    Add files via upload

--- task5/main.cpp
+++ task5/main.cpp
@@ -1,67 +1,92 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
+#include <memory>
 #include <chrono>
-#include <thread>
+#include <vector>
+#include <unistd.h>
+#include <map>
 
-enum LogLevel {
+template<class Derived>
+class singleton {
+public:
+    static Derived *instance() {
+        if (!m_ptr)
+            m_ptr = new Derived();
+        return m_ptr;
+    }
+
+    //attention: no destructor for singleton
+    static void destroy() {
+        delete m_ptr;
+        m_ptr = nullptr;
+    }
+
+private:
+    static Derived *m_ptr;
+};
+
+template<class Derived>
+Derived *singleton<Derived>::m_ptr = nullptr;
+
+// поддерживать 3 уровня важности событий (нормальный, замечание, ошибка);
+enum log_type {
     LOG_NORMAL,
     LOG_WARNING,
     LOG_ERROR
 };
 
-class Log
-{
-    static constexpr int maxLogs = 10;
-    static Log* m_Instance;
-
-    using Clock = std::chrono::steady_clock;
-    Clock::time_point m_Start;
-
-    std::vector<std::tuple<LogLevel, Clock::time_point, std::string>> logs;
-
-    Log()
-            :	m_Start {Clock::now()}
-    {}
-
+class Logger : public singleton<Logger> {
+    friend singleton<Logger>;
 public:
-    static Log* Instance()
-    {
-        if (!m_Instance)
-            m_Instance = new Log;
-        return m_Instance;
+    // обеспечить фиксацию события (с событием фиксируются время, важность, текстовое сообщение);
+    void message(enum log_type type, std::string const &msg) {
+        const auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        rec.push_back(Record{type, time, msg});
+        if (rec.size() > rec_limit) {
+            rec.erase(rec.begin());
+        }
     }
 
-    void message(LogLevel level, const std::string& message)
-    {
-        logs.push_back(std::make_tuple(level, std::chrono::steady_clock::now(), message));
-
-        if (logs.size() > maxLogs)
-            logs.erase(logs.begin());
+    // выводить на печать 10 последних событий.
+    void print() {
+        for (auto i: rec) {
+            std::cout << text_type[i.type] << "\t"
+                      << std::ctime(&i.time) << "\t"
+                      << i.msg << std::endl;
+        }
     }
 
-    void print()
-    {
-        using namespace std::chrono;
-        for (const auto& log : logs)
-            std::cout << "[" << std::get<0>(log) << "] " << (duration_cast<milliseconds>(std::get<1>(log) - m_Start)).count() << ": " << std::get<2>(log) << "\n";
-    }
+private:
+    //constructor must be private in order to forbid call without instance()
+    Logger() {}
+
+    struct Record {
+        enum log_type type;
+        std::time_t time;
+        std::string msg;
+    };
+
+    std::vector<Record> rec;
+    std::size_t rec_limit = 10;
+
+    std::map<enum log_type, std::string> text_type{
+            std::pair{log_type::LOG_NORMAL, "normal"},
+            std::pair{log_type::LOG_WARNING, "warning"},
+            std::pair{log_type::LOG_ERROR, "error"}};
 };
 
-Log* Log::m_Instance = nullptr;
 
-int main()
-{
-    using namespace std::chrono;
-    Log *log = Log::Instance();
+int main() {
+    Logger *log = Logger::instance();
+    log->message(LOG_NORMAL, "Hello, world!");
+    log->message(LOG_NORMAL, "123");
+
+    sleep(1);
+
     log->message(LOG_NORMAL, "program loaded");
-    log->message(LOG_NORMAL, "point 1");
-    std::this_thread::sleep_for(1ms);
-    log->message(LOG_NORMAL, "point 2");
-    std::this_thread::sleep_for(3ms);
-    log->message(LOG_NORMAL, "point 3");
-    std::this_thread::sleep_for(1s);
+    log->message(LOG_WARNING, "warning! pay attention");
     log->message(LOG_ERROR, "error happens! help me!");
     log->print();
+
+    Logger::destroy();
     return 0;
 }
